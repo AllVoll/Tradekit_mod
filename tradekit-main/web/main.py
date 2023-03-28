d04a7cab7610872fc86f068d8f39b92d56d98be8
AllVoll <128129005+AllVoll@users.noreply.github.com>
2023-03-28 13:11:41 +0300

    Add files via upload

---
+++ tradekit-main/web/main.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.api.api_manager import router as api_key_router
+import asyncpg
+
+
+app = FastAPI()
+
+@app.post("/api-keys/")
+async def create_api_key(request: Request, 
+                         name: str = Form(...), 
+                         binance_key: str = Form(...), 
+                         binance_secret: str = Form(...)):
+    try:
+        conn = await asyncpg.connect(user="tradekit", password="yourpassword", database="tradekit", host="localhost")
+        query = "INSERT INTO api_keys (name, binance_key, binance_secret) VALUES ($1, $2, $3)"
+        await conn.execute(query, name, binance_key, binance_secret)
+    finally:
+        await conn.close()
+    return {"status": "success"}
+
+@app.post("/api-keys/")
+async def create_api_key(request: Request, 
+                         name: str = Form(...), 
+                         binance_key: str = Form(...), 
+                         binance_secret: str = Form(...)):
+    conn = None
+    try:
+        conn = await asyncpg.connect(user="tradekit", password="yourpassword", database="tradekit", host="localhost")
+        query = "INSERT INTO api_keys (name, binance_key, binance_secret) VALUES ($1, $2, $3)"
+        await conn.execute(query, name, binance_key, binance_secret)
+    finally:
+        if conn:
+            await conn.close()
+    return {"status": "success"}"""
+        <html>
+            <head>
+                <title>API Keys</title>
+            </head>
+            <body>
+                <h1>API Keys</h1>
+                <form method="post">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" required><br>
+                    <label for="binance_key">Binance API Key:</label>
+                    <input type="text" name="binance_key" required><br>
+                    <label for="binance_secret">Binance API Secret:</label>
+                    <input type="text" name="binance_secret" required><br>
+                    <input type="submit" value="Create">
+                </form>
+                <h2>Existing Keys:</h2>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Binance API Key</th>
+                        <th>Binance API Secret</th>
+                    </tr>
+                    %s
+                </table>
+            </body>
+        </html>
+    """ % "\n".join(f"<tr><td>{row['id']}</td><td>{row['name']}</td><td>{row['binance_key']}</td><td>{row['binance_secret']}</td></tr>" for row in result)
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/tradingview_widget")
+async def tradingview_widget(request: Request):
+    return templates.TemplateResponse("tradingview_widget.html", {"request": request})
+
+@app.get("/settings")
+async def tradingview_widget(request: Request):
+    return templates.TemplateResponse("settings.html", {"request": request})
+
+@app.get("/settings/api_manager")
+async def api_manager(request: Request):
+    return templates.TemplateResponse("api_manager.html", {"request": request})
+
+@app.get("/settings/coinlist_manager")
+async def coinlist_manager(request: Request):
+    return templates.TemplateResponse("coinlist_manager.html", {"request": request})
+
+@app.get("/settings/depo_manager")
+async def depo_manager(request: Request):
+    return templates.TemplateResponse("depo_manager.html", {"request": request})
+
+@app.get("/settings/strategy_manager")
+async def strategy_manager(request: Request):
+    return templates.TemplateResponse("strategy_manager.html", {"request": request})    
